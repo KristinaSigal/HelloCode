f2eeea48d3bfb3b86d905171b990d21df3ba9c53
kristina.sigal <kulrikh@gmail.com>
2022-08-07 03:05:02 +0300

    Add Random

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,4 +1,6 @@
-﻿int numberA = 31;
-int numberB = 5;
+﻿int numberA = new Random().Next(1, 10); // значение numberA от 1 до 9
+Console.WriteLine(numberA); //выведение в терминале значения numberA
+int numberB = new Random().Next(1, 10); 
+Console.WriteLine(numberB); //выведение в терминале значения numberB
 int result = numberA + numberB;
-Console.WriteLine(result);
+Console.WriteLine(result); // выведение в терминале результата сложения
